50805ead7a4550a775e2f07fb9976185706abe31
Kit <b@yorkemail.org>
2023-09-15 11:50:58 +1200

    various code cleanups as suggested by rustrover

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ use gettextrs::*;
 use iced_style::theme::Palette;
 use serde_derive::{Serialize, Deserialize};
 use toml;
-use libstyle::{col_from_str, ButtonStyle, ThemeFile, col_from_string, ThemeSet, CustomTheme, mk_app_theme};
-mod libstyle;
+use lib_style::{col_from_str, ButtonStyle, ThemeFile, col_from_string, ThemeSet, CustomTheme, mk_app_theme};
+mod lib_style;
 
 fn main() -> Result {
     let _ = textdomain("TetraTheme");
@@ -17,7 +17,7 @@ fn main() -> Result {
 }
 const COLOR_SIZE: u16 = 50;
 
-fn buttonstyle_from_col(color: &Color) -> ButtonStyle{
+fn button_style_from_col(color: &Color) -> ButtonStyle{
     ButtonStyle { border_radius: 2.5, txt_color: color.clone(), bg_color: Some(color.clone()), border_color: color.clone(), border_width: 0.0, shadow_offset: iced::Vector { x: 0.0, y: 0.0 } }
 }
 
@@ -46,11 +46,11 @@ enum ThemeType {
     Dark,
     Custom
 }
-fn generate_theme(themeslot: ThemeType) -> Option<CustomTheme> {
-    match themeslot {
+fn generate_theme(theme_slot: ThemeType) -> Option<CustomTheme> {
+    match theme_slot {
         ThemeType::Light => {
             Some(CustomTheme {
-                application: iced::theme::Palette {
+                application: Palette {
                     background: Color::from_rgb8(0xE0, 0xF5, 0xFF),
                     text: Color::from_rgb8(0x00, 0x19, 0x36),
                     primary: Color::from_rgb8(0x00, 0xF1, 0xD6),
@@ -77,7 +77,7 @@ fn generate_theme(themeslot: ThemeType) -> Option<CustomTheme> {
         }
         ThemeType::Dark => {
             Some(CustomTheme { // TODO: set dark theme properly
-                application: iced::theme::Palette {
+                application: Palette {
                     background: Color::from_rgb8(0x00, 0x19, 0x36),
                     text: Color::from_rgb8(0xE0, 0xF5, 0xFF),
                     primary: Color::from_rgb8(0x00, 0xCD, 0xB6),
@@ -141,10 +141,10 @@ fn format_radix(mut x: u32, radix: u32) -> String {
 }
 fn string_from_col(color: &Color) -> String {
     let rgba = color.into_rgba8();
-    let redstr = format_radix(rgba[0].into(), 16);
-    let greenstr = format_radix(rgba[1].into(), 16);
-    let bluestr = format_radix(rgba[2].into(), 16);
-    format!("{redstr}{greenstr}{bluestr}")
+    let red_str = format_radix(rgba[0].into(), 16);
+    let green_str = format_radix(rgba[1].into(), 16);
+    let blue_str = format_radix(rgba[2].into(), 16);
+    format!("{red_str}{green_str}{blue_str}")
 }
 
 #[derive(Debug, Clone)]
@@ -180,7 +180,7 @@ fn get_config_home() -> String {
 impl Default for Configurator {
     fn default() -> Self {
         let path = format!("{}/Oceania/theme.toml", get_config_home());
-        let themefile: Option<ThemeFile> = match std::fs::read_to_string(path) {
+        let theme_file: Option<ThemeFile> = match std::fs::read_to_string(path) {
             Ok(value) => {
                 Some(toml::from_str(&value).unwrap())
             }
@@ -188,7 +188,7 @@ impl Default for Configurator {
                 None
             }
         };
-        match themefile {
+        match theme_file {
             Some(value) => {
                 Configurator {
                     bg1: col_from_string(value.bg_color1.clone()),
@@ -286,9 +286,9 @@ impl Default for Configurator {
 }
 
 impl Application for Configurator {
+    type Executor = iced::executor::Default;
     type Message = Message;
     type Theme = Theme;
-    type Executor = iced::executor::Default;
     type Flags = ();
     fn new(_flags: ()) -> (Self, iced::Command<Message>) {
         (Configurator::default(), iced::Command::none())
@@ -330,95 +330,95 @@ impl Application for Configurator {
                     pink: string_from_col(&self.pink),
                 };
                 let path = format!("{}/Oceania/theme.toml", get_config_home());
-                let tomlout = toml::to_string(&file).unwrap();
-                let _ = std::fs::write(path, tomlout);
+                let toml_out = toml::to_string(&file).unwrap();
+                let _ = std::fs::write(path, toml_out);
             }
         }
         iced::Command::none()
     }
     fn view(&self) -> iced::Element<'_, Self::Message> {
-        let bg1but = Button::new("").on_press(Message::OpenPicker(ColorSlot::Bg1)).width(COLOR_SIZE).height(COLOR_SIZE).style(buttonstyle_from_col(&self.bg1).mk_theme());
-        let bg1picker = ColorPicker::new(match &self.open_picker {
+        let bg1_but = Button::new("").on_press(Message::OpenPicker(ColorSlot::Bg1)).width(COLOR_SIZE).height(COLOR_SIZE).style(button_style_from_col(&self.bg1).mk_theme());
+        let bg1_picker = ColorPicker::new(match &self.open_picker {
             Some(value) => value == &ColorSlot::Bg1,
             None => false
-        }, self.bg1, bg1but, Message::ClosePicker, Message::SubmitColor);
+        }, self.bg1, bg1_but, Message::ClosePicker, Message::SubmitColor);
         let bg1label = Text::new(gettext("Primary Background Color"));
-        let bg1row = Row::new().push(bg1label).push(Space::new(Length::Fill, 10)).push(bg1picker).align_items(iced::Alignment::Center);
-        let bg2but = Button::new("").on_press(Message::OpenPicker(ColorSlot::Bg2)).width(COLOR_SIZE).height(COLOR_SIZE).style(buttonstyle_from_col(&self.bg2).mk_theme());
-        let bg2picker = ColorPicker::new(match &self.open_picker {
+        let bg1_row = Row::new().push(bg1label).push(Space::new(Length::Fill, 10)).push(bg1_picker).align_items(iced::Alignment::Center);
+        let bg2_but = Button::new("").on_press(Message::OpenPicker(ColorSlot::Bg2)).width(COLOR_SIZE).height(COLOR_SIZE).style(button_style_from_col(&self.bg2).mk_theme());
+        let bg2_picker = ColorPicker::new(match &self.open_picker {
             Some(value) => value == &ColorSlot::Bg2,
             None => false
-        }, self.bg2, bg2but, Message::ClosePicker, Message::SubmitColor);
+        }, self.bg2, bg2_but, Message::ClosePicker, Message::SubmitColor);
         let bg2label = Text::new(gettext("Secondary Background Color"));
-        let bg2row = Row::new().push(bg2label).push(Space::new(Length::Fill, 10)).push(bg2picker).align_items(iced::Alignment::Center).spacing(10);
-        let bg3but = Button::new("").on_press(Message::OpenPicker(ColorSlot::Bg3)).width(COLOR_SIZE).height(COLOR_SIZE).style(buttonstyle_from_col(&self.bg3).mk_theme());
-        let bg3picker = ColorPicker::new(match &self.open_picker {
+        let bg2_row = Row::new().push(bg2label).push(Space::new(Length::Fill, 10)).push(bg2_picker).align_items(iced::Alignment::Center).spacing(10);
+        let bg3_but = Button::new("").on_press(Message::OpenPicker(ColorSlot::Bg3)).width(COLOR_SIZE).height(COLOR_SIZE).style(button_style_from_col(&self.bg3).mk_theme());
+        let bg3_picker = ColorPicker::new(match &self.open_picker {
             Some(value) => value == &ColorSlot::Bg3,
             None => false
-        }, self.bg3, bg3but, Message::ClosePicker, Message::SubmitColor);
+        }, self.bg3, bg3_but, Message::ClosePicker, Message::SubmitColor);
         let bg3label = Text::new(gettext("Tertiary Background Color"));
-        let bg3row = Row::new().push(bg3label).push(Space::new(Length::Fill, 10)).push(bg3picker).align_items(iced::Alignment::Center).spacing(10);
-        let txtbut = Button::new("").on_press(Message::OpenPicker(ColorSlot::Txt)).width(COLOR_SIZE).height(COLOR_SIZE).style(buttonstyle_from_col(&self.txt).mk_theme());
-        let txtpicker = ColorPicker::new(match &self.open_picker {
+        let bg3_row = Row::new().push(bg3label).push(Space::new(Length::Fill, 10)).push(bg3_picker).align_items(iced::Alignment::Center).spacing(10);
+        let txt_but = Button::new("").on_press(Message::OpenPicker(ColorSlot::Txt)).width(COLOR_SIZE).height(COLOR_SIZE).style(button_style_from_col(&self.txt).mk_theme());
+        let txt_picker = ColorPicker::new(match &self.open_picker {
             Some(value) => value == &ColorSlot::Txt,
             None => false
-        }, self.txt, txtbut, Message::ClosePicker, Message::SubmitColor);
-        let txtlabel = Text::new(gettext("Text Color"));
-        let txtrow = Row::new().push(txtlabel).push(Space::new(Length::Fill, 10)).push(txtpicker).align_items(iced::Alignment::Center).spacing(10);
-        let redbut = Button::new("").on_press(Message::OpenPicker(ColorSlot::Red)).width(COLOR_SIZE).height(COLOR_SIZE).style(buttonstyle_from_col(&self.red).mk_theme());
-        let redpicker = ColorPicker::new(match &self.open_picker {
+        }, self.txt, txt_but, Message::ClosePicker, Message::SubmitColor);
+        let txt_label = Text::new(gettext("Text Color"));
+        let txt_row = Row::new().push(txt_label).push(Space::new(Length::Fill, 10)).push(txt_picker).align_items(iced::Alignment::Center).spacing(10);
+        let red_but = Button::new("").on_press(Message::OpenPicker(ColorSlot::Red)).width(COLOR_SIZE).height(COLOR_SIZE).style(button_style_from_col(&self.red).mk_theme());
+        let red_picker = ColorPicker::new(match &self.open_picker {
             Some(value) => value == &ColorSlot::Red,
             None => false
-        }, self.red, redbut, Message::ClosePicker, Message::SubmitColor);
-        let redlabel = Text::new(gettext("Red Color"));
-        let redrow = Row::new().push(redlabel).push(Space::new(Length::Fill, 10)).push(redpicker).align_items(iced::Alignment::Center).spacing(10);
-        let orangebut = Button::new("").on_press(Message::OpenPicker(ColorSlot::Orange)).width(COLOR_SIZE).height(COLOR_SIZE).style(buttonstyle_from_col(&self.orange).mk_theme());
-        let orangepicker = ColorPicker::new(match &self.open_picker {
+        }, self.red, red_but, Message::ClosePicker, Message::SubmitColor);
+        let red_label = Text::new(gettext("Red Color"));
+        let red_row = Row::new().push(red_label).push(Space::new(Length::Fill, 10)).push(red_picker).align_items(iced::Alignment::Center).spacing(10);
+        let orange_but = Button::new("").on_press(Message::OpenPicker(ColorSlot::Orange)).width(COLOR_SIZE).height(COLOR_SIZE).style(button_style_from_col(&self.orange).mk_theme());
+        let orange_picker = ColorPicker::new(match &self.open_picker {
             Some(value) => value == &ColorSlot::Orange,
             None => false
-        }, self.orange, orangebut, Message::ClosePicker, Message::SubmitColor);
-        let orangelabel = Text::new(gettext("Orange Color"));
-        let orangerow = Row::new().push(orangelabel).push(Space::new(Length::Fill, 10)).push(orangepicker).align_items(iced::Alignment::Center).spacing(10);
-        let yellowbut = Button::new("").on_press(Message::OpenPicker(ColorSlot::Yellow)).width(COLOR_SIZE).height(COLOR_SIZE).style(buttonstyle_from_col(&self.yellow).mk_theme());
-        let yellowpicker = ColorPicker::new(match &self.open_picker {
+        }, self.orange, orange_but, Message::ClosePicker, Message::SubmitColor);
+        let orange_label = Text::new(gettext("Orange Color"));
+        let orange_row = Row::new().push(orange_label).push(Space::new(Length::Fill, 10)).push(orange_picker).align_items(iced::Alignment::Center).spacing(10);
+        let yellow_but = Button::new("").on_press(Message::OpenPicker(ColorSlot::Yellow)).width(COLOR_SIZE).height(COLOR_SIZE).style(button_style_from_col(&self.yellow).mk_theme());
+        let yellow_picker = ColorPicker::new(match &self.open_picker {
             Some(value) => value == &ColorSlot::Yellow,
             None => false
-        }, self.yellow, yellowbut, Message::ClosePicker, Message::SubmitColor);
-        let yellowlabel = Text::new(gettext("Yellow Color"));
-        let yellowrow = Row::new().push(yellowlabel).push(Space::new(Length::Fill, 10)).push(yellowpicker).align_items(iced::Alignment::Center).spacing(10);
-        let greenbut = Button::new("").on_press(Message::OpenPicker(ColorSlot::Green)).width(COLOR_SIZE).height(COLOR_SIZE).style(buttonstyle_from_col(&self.green).mk_theme());
-        let greenpicker = ColorPicker::new(match &self.open_picker {
+        }, self.yellow, yellow_but, Message::ClosePicker, Message::SubmitColor);
+        let yellow_label = Text::new(gettext("Yellow Color"));
+        let yellow_row = Row::new().push(yellow_label).push(Space::new(Length::Fill, 10)).push(yellow_picker).align_items(iced::Alignment::Center).spacing(10);
+        let green_but = Button::new("").on_press(Message::OpenPicker(ColorSlot::Green)).width(COLOR_SIZE).height(COLOR_SIZE).style(button_style_from_col(&self.green).mk_theme());
+        let green_picker = ColorPicker::new(match &self.open_picker {
             Some(value) => value == &ColorSlot::Green,
             None => false
-        }, self.green, greenbut, Message::ClosePicker, Message::SubmitColor);
-        let greenlabel = Text::new(gettext("Green Color"));
-        let greenrow = Row::new().push(greenlabel).push(Space::new(Length::Fill, 10)).push(greenpicker).align_items(iced::Alignment::Center).spacing(10);
-        let bluebut = Button::new("").on_press(Message::OpenPicker(ColorSlot::Blue)).width(COLOR_SIZE).height(COLOR_SIZE).style(buttonstyle_from_col(&self.blue).mk_theme());
-        let bluepicker = ColorPicker::new(match &self.open_picker {
+        }, self.green, green_but, Message::ClosePicker, Message::SubmitColor);
+        let green_label = Text::new(gettext("Green Color"));
+        let green_row = Row::new().push(green_label).push(Space::new(Length::Fill, 10)).push(green_picker).align_items(iced::Alignment::Center).spacing(10);
+        let blue_but = Button::new("").on_press(Message::OpenPicker(ColorSlot::Blue)).width(COLOR_SIZE).height(COLOR_SIZE).style(button_style_from_col(&self.blue).mk_theme());
+        let blue_picker = ColorPicker::new(match &self.open_picker {
             Some(value) => value == &ColorSlot::Blue,
             None => false
-        }, self.blue, bluebut, Message::ClosePicker, Message::SubmitColor);
-        let bluelabel = Text::new(gettext("Blue Color"));
-        let bluerow = Row::new().push(bluelabel).push(Space::new(Length::Fill, 10)).push(bluepicker).align_items(iced::Alignment::Center).spacing(10);
-        let purplebut = Button::new("").on_press(Message::OpenPicker(ColorSlot::Purple)).width(COLOR_SIZE).height(COLOR_SIZE).style(buttonstyle_from_col(&self.purple).mk_theme());
-        let purplepicker = ColorPicker::new(match &self.open_picker {
+        }, self.blue, blue_but, Message::ClosePicker, Message::SubmitColor);
+        let blue_label = Text::new(gettext("Blue Color"));
+        let blue_row = Row::new().push(blue_label).push(Space::new(Length::Fill, 10)).push(blue_picker).align_items(iced::Alignment::Center).spacing(10);
+        let purple_but = Button::new("").on_press(Message::OpenPicker(ColorSlot::Purple)).width(COLOR_SIZE).height(COLOR_SIZE).style(button_style_from_col(&self.purple).mk_theme());
+        let purple_picker = ColorPicker::new(match &self.open_picker {
             Some(value) => value == &ColorSlot::Purple,
             None => false
-        }, self.purple, purplebut, Message::ClosePicker, Message::SubmitColor);
-        let purplelabel = Text::new(gettext("Purple Color"));
-        let purplerow = Row::new().push(purplelabel).push(Space::new(Length::Fill, 10)).push(purplepicker).align_items(iced::Alignment::Center).spacing(10);
-        let pinkbut = Button::new("").on_press(Message::OpenPicker(ColorSlot::Pink)).width(COLOR_SIZE).height(COLOR_SIZE).style(buttonstyle_from_col(&self.pink).mk_theme());
-        let pinkpicker = ColorPicker::new(match &self.open_picker {
+        }, self.purple, purple_but, Message::ClosePicker, Message::SubmitColor);
+        let purple_label = Text::new(gettext("Purple Color"));
+        let purple_row = Row::new().push(purple_label).push(Space::new(Length::Fill, 10)).push(purple_picker).align_items(iced::Alignment::Center).spacing(10);
+        let pink_but = Button::new("").on_press(Message::OpenPicker(ColorSlot::Pink)).width(COLOR_SIZE).height(COLOR_SIZE).style(button_style_from_col(&self.pink).mk_theme());
+        let pink_picker = ColorPicker::new(match &self.open_picker {
             Some(value) => value == &ColorSlot::Pink,
             None => false
-        }, self.pink, pinkbut, Message::ClosePicker, Message::SubmitColor);
-        let pinklabel = Text::new(gettext("Pink Color"));
-        let pinkrow = Row::new().push(pinklabel).push(Space::new(Length::Fill, 10)).push(pinkpicker).align_items(iced::Alignment::Center).spacing(10);
+        }, self.pink, pink_but, Message::ClosePicker, Message::SubmitColor);
+        let pink_label = Text::new(gettext("Pink Color"));
+        let pink_row = Row::new().push(pink_label).push(Space::new(Length::Fill, 10)).push(pink_picker).align_items(iced::Alignment::Center).spacing(10);
         let save = Button::new(Text::new(gettext("Save"))).on_press(Message::Save);
-        let saverow = Row::new().align_items(iced::Alignment::Start).push(save).push(Space::new(Length::Fill, 10));
+        let save_row = Row::new().align_items(iced::Alignment::Start).push(save).push(Space::new(Length::Fill, 10));
 
-        let master = Column::new().push(bg1row).push(bg2row).push(bg3row).push(txtrow).push(redrow).push(orangerow).push(yellowrow).push(greenrow).push(bluerow).push(purplerow).push(pinkrow).push(saverow).align_items(iced::Alignment::Center).spacing(10);
-        Container::new(master).center_x().center_y().width(iced::Length::Fill).height(iced::Length::Fill).into()
+        let master = Column::new().push(bg1_row).push(bg2_row).push(bg3_row).push(txt_row).push(red_row).push(orange_row).push(yellow_row).push(green_row).push(blue_row).push(purple_row).push(pink_row).push(save_row).align_items(iced::Alignment::Center).spacing(10);
+        Container::new(master).center_x().center_y().width(Length::Fill).height(Length::Fill).into()
     }
     fn theme(&self) -> Self::Theme {
         match self.theme_type {
